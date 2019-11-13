59e88db2c0ad7a3c3dd08a9226d6e6dc431d3639
royeden <roy.ede@gmail.com>
2019-11-13 17:29:47 -0300

    Finished base site & server

--- server.js
+++ server.js
@@ -3,15 +3,26 @@ const serveStatic = require('serve-static');
 
 const app = express();
 
-let percentage = 10;
+let percentage = 0;
 
 app.use(serveStatic('static'));
 
 app.get('/update', (_, res) => {
-  res.set('Content-Type', 'text/html')
   res.json({
     percentage
   });
+});
+
+app.put('/increase', (req, res) => {
+  console.log(`Received increase request from ${req.username}`);
+  if (percentage < 100) percentage++;
+  res.send();
+});
+
+app.put('/decrease', (req, res) => {
+  console.log(`Received decrease request from ${req.params.username}`);
+  if (percentage > 0) percentage--;
+  res.send();
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
