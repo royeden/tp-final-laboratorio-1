7cb272ce65c61bf001f83ce4759e0a5de0366d0f
RoyEde <RoyEden>
2019-11-14 18:48:04 -0300

    Configurable language options

--- config.js
+++ config.js
@@ -1,3 +1,42 @@
+const en = {
+  boundaries: {
+    max: ', but percentage is at max!',
+    min: ', but percentage is at min!',
+    fallback: '!'
+  },
+  initialMessage: port => `Initiated server in port ${port}!`,
+  logSaved: 'Updated log!',
+  percentageIsAt: percentage => `Percentage is at ${percentage}%`,
+  receivedRequest: (type, from) => `Received ${type} request from ${from}`,
+  registeredUser: (id, username) => `Registered the user ${id}_${username}`,
+  types: {
+    decrease: 'decrease',
+    increase: 'increase'
+  }
+};
+
+const es = {
+  boundaries: {
+    max: ', pero el porcentaje está al máximo!',
+    min: ', pero el porcentaje está al mínimo!',
+    fallback: '!'
+  },
+  initialMessage: port => `¡Servidor iniciado en el puerto ${port}!`,
+  logSaved: '¡Datos actualizados!',
+  percentageIsAt: percentage => `El porcentaje está al ${percentage}%`,
+  receivedRequest: (type, from) =>
+  `¡Recibido pedido de ${type} realizado por ${from}`,
+  registeredUser: (id, username) => `Se registró el usuario ${id}_${username}`,
+  types: {
+    decrease: 'disminuir',
+    increase: 'incrementar'
+  }
+};
+
 exports.config = {
-  port: 3000
-}
+  port: 3000,
+  strings: {
+    en,
+    es
+  }
+};
--- server.js
+++ server.js
@@ -1,9 +1,18 @@
+require('dotenv').config();
+
 const bodyParser = require('body-parser');
+const chalk = require('chalk');
 const express = require('express');
 const fs = require('fs');
 const serveStatic = require('serve-static');
 
 const { config } = require('./config');
+
+const { LANGUAGE_CONFIG } = process.env;
+
+const { port, strings: configStrings } = config;
+
+const strings = configStrings[LANGUAGE_CONFIG.toLowerCase()];
 
 const app = express();
 
@@ -15,34 +24,39 @@ const fsCallback = error => {
     return console.error(error);
   }
 
-  console.log('Log was saved!');
+  console.log(strings.logSaved);
 };
 
-fs.writeFile('log.txt', 'Initiated Log!\nPercentage is at 0%\n', fsCallback);
-
-const fsAppend = (log, percentage) => {
-  console.log(log);
-  fs.appendFile(
-    'log.txt',
-    `\n${log}\n\nPercentage is at ${percentage}%\n`,
-    fsCallback
-  );
-};
+fs.writeFile(
+  'log.txt',
+  `${strings.initialMessage(port)}\n${strings.percentageIsAt(percentage)}\n`,
+  fsCallback
+);
 
 app.use(bodyParser.json());
 
 app.use(serveStatic('static'));
 
-app.post('/id', (req, res) => {
+const handleUpdateRequest = (log, callback) => (req, res) => {
   const username = req.body.username;
-  console.log(username)
-  const log = `\nRegistered ${user_id}_${username}\n`;
-  fs.appendFile('log.txt', log, fsCallback);
-  res.json({
-    user_id
-  });
-  ++user_id;
-});
+  const logWithUser = log(username);
+  console.log(logWithUser);
+  fs.appendFile('log.txt', `\n${logWithUser}\n`, fsCallback);
+  callback(req, res, logWithUser);
+};
+
+app.post(
+  '/id',
+  handleUpdateRequest(
+    username => strings.registeredUser(user_id, username),
+    (_, res) => {
+      res.json({
+        user_id
+      });
+      ++user_id;
+    }
+  )
+);
 
 app.get('/update', (_, res) => {
   res.json({
@@ -50,30 +64,46 @@ app.get('/update', (_, res) => {
   });
 });
 
-app.put('/increase', (req, res) => {
-  const username = req.body.username;
-  const log = `Received increase request from ${username}`;
-  if (percentage < 100) {
-    ++percentage;
-    fsAppend(log, percentage);
-    res.send(log);
-  } else {
-    fsAppend(`${log}, but percentage is at max!`, percentage);
-    res.send(`${log}, but percentage is at max!`);
-  }
-});
+app.put(
+  '/increase',
+  handleUpdateRequest(
+    username =>
+      `${strings.receivedRequest(strings.types.increase, username)}${
+        percentage < 100
+          ? `${strings.boundaries.fallback}\n${strings.percentageIsAt(
+              percentage + 1
+            )}`
+          : strings.boundaries.max
+      }`,
+    (_, res, log) => {
+      if (percentage < 100) {
+        ++percentage;
+      }
+      res.send(log);
+    }
+  )
+);
 
-app.put('/decrease', (req, res) => {
-  const username = req.body.username;
-  const log = `Received decrease request from ${username}`;
-  if (percentage > 0) {
-    --percentage;
-    fsAppend(log, percentage);
-    res.send(log);
-  } else {
-    fsAppend(`${log}, but percentage is at min!`, percentage);
-    res.send(`${log}, but percentage is at min!`);
-  }
-});
+app.put(
+  '/decrease',
+  handleUpdateRequest(
+    username =>
+      `${strings.receivedRequest(strings.types.decrease, username)}${
+        percentage > 0
+          ? `${strings.boundaries.fallback}\n${strings.percentageIsAt(
+              percentage - 1
+            )}`
+          : strings.boundaries.min
+      }`,
+    (_, res, log) => {
+      if (percentage > 0) {
+        --percentage;
+      }
+      res.send(log);
+    }
+  )
+);
 
-app.listen(config.port, () => console.log('Example app listening on port 3000!'));
+app.listen(port, () =>
+  console.log(chalk.bold(chalk.green(strings.initialMessage(port))))
+);
