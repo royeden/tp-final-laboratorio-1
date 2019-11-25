0e14b8b1424e4bb39ef9cfa025144ebdb5a53b40
RoyEde <RoyEden>
2019-11-25 11:41:36 -0300

    Updated qr

--- qr.js
+++ qr.js
@@ -6,30 +6,21 @@ const { patch, port } = config;
 
 module.exports = () => {
   const adminAddress = `http://192.168.${patch.local_ip}:${port}/admin`;
-  QRCode.toString(
-    adminAddress,
-    {
-      errorCorrectionLevel: 'H',
-      type: 'terminal'
-    },
-    (error, string) => {
-      if (error) throw new Error(error);
-      console.log(`\n${adminAddress}\n`);
-      console.log(string);
-    }
-    );
-    
+  const timeoutAddress = `http://192.168.${patch.local_ip}:${port}/timeout`;
   const viewerAddress = `http://192.168.${patch.local_ip}:${port}/`;
-  QRCode.toString(
-    viewerAddress,
-    {
-      errorCorrectionLevel: 'H',
-      type: 'terminal'
-    },
-    (error, string) => {
-      if (error) throw new Error(error);
-      console.log(`\n${viewerAddress}\n`);
-      console.log(string);
-    }
+
+  [adminAddress, timeoutAddress, viewerAddress].forEach(adress =>
+    QRCode.toString(
+      adress,
+      {
+        errorCorrectionLevel: 'H',
+        type: 'terminal'
+      },
+      (error, string) => {
+        if (error) throw new Error(error);
+        console.log(`\n${adress}\n`);
+        console.log(string);
+      }
+    )
   );
-}
+};
