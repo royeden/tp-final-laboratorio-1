4efaaa651f371a3de42a19bfb94bd69048d8b1ff
RoyEden <Roy.Ede@gmail.com>
2019-12-10 08:58:55 -0300

    Added ads

---
+++ client/src/images/index.js
@@ -0,0 +1,27 @@
+import png1 from './1.png';
+import png2 from './2.png';
+import png3 from './3.png';
+import png4 from './4.png';
+import png5 from './5.png';
+import png6 from './6.png';
+import png7 from './7.png';
+import png8 from './8.png';
+import png9 from './9.png';
+import png10 from './10.png';
+import png11 from './11.png';
+import png12 from './12.png';
+
+export default [
+  png1,
+  png2,
+  png3,
+  png4,
+  png5,
+  png6,
+  png7,
+  png8,
+  png9,
+  png10,
+  png11,
+  png12
+];
