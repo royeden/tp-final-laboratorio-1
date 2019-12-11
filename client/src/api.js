1cafb91f7664b14a1691b9b2dde672ed97e0be05
RoyEden <Roy.Ede@gmail.com>
2019-12-11 06:26:43 -0300

    Final modal details

--- client/src/api.js
+++ client/src/api.js
@@ -1,5 +1,6 @@
 const mocks = {
-  time: 180000,
+  // time: 180000,
+  time: 10000,
   percentage: 0,
   started: false
 };
@@ -30,7 +31,7 @@ const api = {
     ? mockRequest('/id', { id: 0 })
     : () =>
         fetch('/id', {
-          method: 'GET',
+          method: 'GET'
         }),
   getPercentage: shouldUseMock
     ? mockRequest(
@@ -39,9 +40,10 @@ const api = {
         true,
         500
       )
-    : () => fetch('/percentage', {
-        method: 'GET'
-      }),
+    : () =>
+        fetch('/percentage', {
+          method: 'GET'
+        }),
   getTime: shouldUseMock
     ? mockRequest(
         '/time',
@@ -53,9 +55,46 @@ const api = {
         true,
         500
       )
-    : () => fetch('/time', {
-        method: 'GET'
-      }),
+    : () =>
+        fetch('/time', {
+          method: 'GET'
+        }),
+  postLike: shouldUseMock
+    ? mockRequest(
+        '/like',
+        user => {
+          return {
+            body: `${user} liked the experience!`,
+            ok: true
+          };
+        },
+        false
+      )
+    : user =>
+        fetch('/like', {
+          method: 'POST',
+          ...jsonRequestBody({
+            user
+          })
+        }),
+  postDislike: shouldUseMock
+    ? mockRequest(
+        '/dislike',
+        user => {
+          return {
+            body: `${user} disliked the experience!`,
+            ok: true
+          };
+        },
+        false
+      )
+    : user =>
+        fetch('/dislike', {
+          method: 'POST',
+          ...jsonRequestBody({
+            user
+          })
+        }),
   postPercentageDecrease: shouldUseMock
     ? mockRequest(
         '/decrease',
--- client/src/components/FinalModal.js
+++ client/src/components/FinalModal.js
@@ -2,7 +2,11 @@ import React, { useContext, useEffect, useState } from 'react';
 import styled from 'styled-components';
 // import PropTypes from 'prop-types';
 
+import api from '../api';
+import usePreviousHook from '../hooks/usePreviousHook';
 import { clockContext } from '../context/ClockProvider';
+import { percentageContext } from '../context/PercentageProvider';
+import { userContext } from '../context/UserProvider';
 
 import Modal from './Modal';
 import Button from './Button';
@@ -11,9 +15,19 @@ const ModalTitle = styled.h1`
   text-align: center;
 `;
 
+const ModalDescription = styled.h4`
+  margin: 1rem 0;
+  text-align: center;
+`;
+
 const ModalButtons = styled.div`
   display: flex;
   justify-content: space-around;
+
+  button {
+    flex: 1;
+    margin: 0 1rem;
+  }
 `;
 
 const Container = styled.div`
@@ -25,19 +39,45 @@ const Container = styled.div`
 
 const FinalModal = () => {
   const { clock } = useContext(clockContext);
+  const { percentage } = useContext(percentageContext);
+  const { user } = useContext(userContext);
+  const [finalPercentage, setFinalPercentage] = useState(false);
   const [finalModal, setFinalModal] = useState(false);
   const [sentFinalModal, setSentFinalModal] = useState(false);
+
+  const previousClock = usePreviousHook(clock);
   useEffect(() => {
-    if (clock === 1) setFinalModal(true);
-  }, [clock]);
+    if (previousClock === 1 && clock === 0) {
+      setFinalPercentage(percentage);
+      setFinalModal(true);
+    }
+  }, [clock, percentage, previousClock]);
+
+  const handleRequest = ep => () =>
+    ep(user).then(res => res.ok && setSentFinalModal(true));
   return (
     <Modal visible={finalModal}>
       <Container>
         <ModalTitle>¡Gracias por participar!</ModalTitle>
-        <ModalButtons>
-          <Button type="lightblue">No me gustó</Button>
-          <Button type="red">Me gustó</Button>
-        </ModalButtons>
+        {sentFinalModal ? (
+          <>
+            <ModalDescription>¡Muchas gracias por tu respuesta!</ModalDescription>
+            <ModalDescription>Aguardá a que lxs demás terminen de responder...</ModalDescription>
+          </>
+        ) : (
+          <>
+<ModalDescription>Nivel de violencia final: {finalPercentage}%</ModalDescription>
+            <ModalDescription>Contanos tu experiencia:</ModalDescription>
+            <ModalButtons>
+              <Button onClick={handleRequest(api.postDislike)} type="lightblue">
+                No me gustó
+              </Button>
+              <Button onClick={handleRequest(api.postLike)} type="red">
+                Me gustó
+              </Button>
+            </ModalButtons>
+          </>
+        )}
       </Container>
     </Modal>
   );
